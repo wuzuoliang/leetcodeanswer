997ac88dbb8bd8c147e87d01a76113f09e94bee9
吴柞良 <wuzuoliang@192.168.1.3>
2022-02-07 22:48:10 +0800

    add test code

--- 048_旋转图像_test.go
+++ 048_旋转图像_test.go
@@ -1,5 +1,9 @@
 package Code
 
+import (
+	"testing"
+)
+
 /**
 第48题：旋转图像
 给定一个 n × n 的二维矩阵表示一个图像。
@@ -42,3 +46,52 @@ package Code
   [16, 7,10,11]
 ]
 */
+
+func Test48(t *testing.T) {
+	src := [][]int{
+		{1, 2, 3}, {4, 5, 6}, {7, 8, 9},
+	}
+	rotate(src)
+	t.Log(src)
+	rotateNi(src)
+	t.Log(src)
+}
+
+// 顺时针90度
+func rotate(matrix [][]int) {
+	n := len(matrix)
+	for i := 0; i < n; i++ {
+		for j := i; j < n; j++ {
+			matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+		}
+	}
+
+	for i := 0; i < n; i++ {
+		reverse48(matrix[i])
+	}
+}
+
+// 逆时针90度
+func rotateNi(matrix [][]int) {
+	n := len(matrix)
+	for i := 0; i < n; i++ {
+		for j := 0; j < n-i; j++ {
+			matrix[i][j], matrix[n-j-1][n-i-1] = matrix[n-j-1][n-i-1], matrix[i][j]
+		}
+	}
+
+	for i := 0; i < n; i++ {
+		reverse48(matrix[i])
+	}
+}
+
+func reverse48(arr []int) {
+	i := 0
+	j := len(arr) - 1
+
+	for j > i {
+		arr[i], arr[j] = arr[j], arr[i]
+		i++
+		j--
+	}
+}
