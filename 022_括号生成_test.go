b76c30215b83cb08a96df2cbd53255ed19213e92
吴柞良 <wuzuoliang@MacBook-Pro.local>
2022-02-15 23:04:04 +0800

    add test code

--- 022_括号生成_test.go
+++ 022_括号生成_test.go
@@ -2,6 +2,7 @@ package Code
 
 import (
 	. "github.com/smartystreets/goconvey/convey"
+	"strings"
 	"testing"
 )
 
@@ -39,19 +40,38 @@ func Test_generateParenthesis(t *testing.T) {
 }
 func generateParenthesis(n int) []string {
 	if n == 0 {
-		return []string{""}
+		return nil
 	}
-	final := make([]string, 0)
-	i := 0
-	for i < n {
-		tmpLeft := generateParenthesis(i)
-		for _, vl := range tmpLeft {
-			tmpRight := generateParenthesis(n - 1 - i)
-			for _, vr := range tmpRight {
-				final = append(final, "("+vl+")"+vr)
-			}
-		}
-		i++
+	// 回溯算法
+	res := make([]string, 0)
+	track := make([]string, 0, 2*n)
+	// 可用的左括号和右括号数量初始化为 n
+	backtrace22(2*n, n, n, &track, &res)
+	return res
+}
+
+func backtrace22(n, left, right int, track, res *[]string) {
+	// 若左括号剩下的多，说明不合法
+	if left > right {
+		return
 	}
-	return final
+	// 数量小于 0 肯定是不合法的
+	if left < 0 || right < 0 {
+		return
+	}
+	// 当所有括号都恰好用完时，得到一个合法的括号组合
+	if left == 0 && right == 0 {
+		*res = append(*res, strings.Join(*track, ""))
+		return
+	}
+	// 尝试放一个左括号
+	*track = append(*track, "(")
+	// 选择
+	backtrace22(n, left-1, right, track, res)
+	// 撤消选择
+	*track = (*track)[:n-left-right]
+
+	*track = append(*track, ")")
+	backtrace22(n, left, right-1, track, res)
+	*track = (*track)[:n-left-right]
 }
