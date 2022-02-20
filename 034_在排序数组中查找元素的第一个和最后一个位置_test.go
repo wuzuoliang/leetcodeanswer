e092cab9f68d31630afcfcd7e8664088e2749de7
吴柞良 <wuzuoliang@MacBook-Pro.local>
2022-02-20 20:27:24 +0800

    add test code

--- 034_在排序数组中查找元素的第一个和最后一个位置_test.go
+++ 034_在排序数组中查找元素的第一个和最后一个位置_test.go
@@ -6,20 +6,31 @@ import (
 )
 
 /**
-Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
+给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
 
-Your algorithm's runtime complexity must be in the order of O(log n).
+如果数组中不存在目标值 target，返回[-1, -1]。
 
-If the target is not found in the array, return [-1, -1].
+进阶：
 
-Example 1:
+你可以设计并实现时间复杂度为O(log n)的算法解决此问题吗？
 
-Input: nums = [5,7,7,8,8,10], target = 8
-Output: [3,4]
-Example 2:
 
-Input: nums = [5,7,7,8,8,10], target = 6
-Output: [-1,-1]
+示例 1：
+
+输入：nums = [5,7,7,8,8,10], target = 8
+输出：[3,4]
+示例2：
+
+输入：nums = [5,7,7,8,8,10], target = 6
+输出：[-1,-1]
+示例 3：
+
+输入：nums = [], target = 0
+输出：[-1,-1]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/find-first-and-last-position-of-element-in-sorted-array
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 */
 
 func Test_searchRange(t *testing.T) {
@@ -43,7 +54,6 @@ func Test_searchRange(t *testing.T) {
 }
 
 func searchRange(nums []int, target int) []int {
-
 	res := []int{-1, -1}
 	if len(nums) < 1 {
 		return res
