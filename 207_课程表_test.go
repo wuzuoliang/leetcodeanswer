d86c6c6622f55b5d5b5c93a17e77efc24065bee6
吴柞良 <wuzuoliang@MacBook-Pro.local>
2022-03-03 00:25:05 +0800

    add test code

---
+++ 207_课程表_test.go
@@ -0,0 +1,37 @@
+package Code
+
+/**
+你这个学期必须选修 numCourses 门课程，记为0到numCourses - 1 。
+
+在选修某些课程之前需要一些先修课程。 先修课程按数组prerequisites 给出，其中prerequisites[i] = [ai, bi] ，表示如果要学习课程ai 则 必须 先学习课程 bi 。
+
+例如，先修课程对[0, 1] 表示：想要学习课程 0 ，你需要先完成课程 1 。
+请你判断是否可能完成所有课程的学习？如果可以，返回 true ；否则，返回 false 。
+
+
+
+示例 1：
+
+输入：numCourses = 2, prerequisites = [[1,0]]
+输出：true
+解释：总共有 2 门课程。学习课程 1 之前，你需要完成课程 0 。这是可能的。
+示例 2：
+
+输入：numCourses = 2, prerequisites = [[1,0],[0,1]]
+输出：false
+解释：总共有 2 门课程。学习课程 1 之前，你需要先完成​课程 0 ；并且学习课程 0 之前，你还应先完成课程 1 。这是不可能的。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/course-schedule
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+func canFinish(numCourses int, prerequisites [][]int) bool {
+	pic := make(map[int]struct{})
+	for _, l := range prerequisites {
+		pic[l[0]] = struct{}{}
+		pic[l[1]] = struct{}{}
+	}
+	total := len(pic)
+
+}
---
+++ 215_数组中的第K个最大元素_test.go
@@ -0,0 +1,109 @@
+package Code
+
+import (
+	"math/rand"
+	"testing"
+	"time"
+)
+
+/**
+给定整数数组 nums 和整数 k，请返回数组中第 k 个最大的元素。
+
+请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
+
+
+
+示例 1:
+
+输入: [3,2,1,5,6,4] 和 k = 2
+输出: 5
+示例2:
+
+输入: [3,2,3,1,2,4,5,5,6] 和 k = 4
+输出: 4
+
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/kth-largest-element-in-an-array
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+func Test215(t *testing.T) {
+	//t.Log(findKthLargest([]int{3,2,1,5,6,4},2))//5
+	//t.Log(findKthLargest([]int{3,2,3,1,2,4,5,5,6},4))//4
+	t.Log(findKthLargest2([]int{5, 2, 4, 1, 3, 6, 0}, 4)) //3
+}
+
+// 快排
+func findKthLargest(nums []int, k int) int {
+	rand.Seed(time.Now().UnixNano())
+	return quickSort(nums, 0, len(nums)-1, k)
+}
+
+func quickSort(nums []int, low, high, k int) int {
+	pivot := randomPartition(nums, low, high)
+	if pivot == k-1 {
+		return nums[pivot]
+	}
+	if pivot > k-1 {
+		return quickSort(nums, low, pivot-1, k)
+	}
+	return quickSort(nums, pivot+1, high, k)
+}
+
+func randomPartition(nums []int, low, high int) int {
+	i := rand.Int()%(high-low+1) + low
+	nums[i], nums[low] = nums[low], nums[i]
+	return partition(nums, low, high)
+}
+
+func partition(nums []int, low, high int) int {
+	pivot := nums[low]
+
+	for low < high {
+		for low < high && nums[high] <= pivot {
+			high--
+		}
+		nums[low] = nums[high]
+
+		for low < high && nums[low] >= pivot {
+			low++
+		}
+		nums[high] = nums[low]
+	}
+
+	nums[low] = pivot
+
+	return low
+}
+
+// 小顶堆
+func findKthLargest2(nums []int, k int) int {
+	heapSize := len(nums)
+	buildMaxHeap(nums, heapSize)
+	for i := len(nums) - 1; i >= len(nums)-k+1; i-- {
+		nums[0], nums[i] = nums[i], nums[0]
+		heapSize--
+		maxHeapify(nums, 0, heapSize)
+	}
+	return nums[0]
+}
+
+func buildMaxHeap(a []int, heapSize int) {
+	for i := heapSize / 2; i >= 0; i-- {
+		maxHeapify(a, i, heapSize)
+	}
+}
+
+func maxHeapify(a []int, i, heapSize int) {
+	l, r, largest := i*2+1, i*2+2, i
+	if l < heapSize && a[l] > a[largest] {
+		largest = l
+	}
+	if r < heapSize && a[r] > a[largest] {
+		largest = r
+	}
+	if largest != i {
+		a[i], a[largest] = a[largest], a[i]
+		maxHeapify(a, largest, heapSize)
+	}
+}
