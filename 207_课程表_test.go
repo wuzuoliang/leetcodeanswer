d7e386da50d3beb5ae72f13cab03db040e43a954
吴柞良 <wuzuoliang@MacBook-Pro.local>
2022-03-04 00:22:50 +0800

    add test code

--- 207_课程表_test.go
+++ 207_课程表_test.go
@@ -1,5 +1,9 @@
 package Code
 
+import (
+	"testing"
+)
+
 /**
 你这个学期必须选修 numCourses 门课程，记为0到numCourses - 1 。
 
@@ -19,19 +23,112 @@ package Code
 
 输入：numCourses = 2, prerequisites = [[1,0],[0,1]]
 输出：false
-解释：总共有 2 门课程。学习课程 1 之前，你需要先完成​课程 0 ；并且学习课程 0 之前，你还应先完成课程 1 。这是不可能的。
+解释：总共有 2 门课程。学习课程 1 之前，你需要先完成课程 0 ；并且学习课程 0 之前，你还应先完成课程 1 。这是不可能的。
+
+提示：
+
+1 <= numCourses <= 105
+0 <= prerequisites.length <= 5000
+prerequisites[i].length == 2
+0 <= ai, bi < numCourses
+prerequisites[i] 中的所有课程对 互不相同
 
 来源：力扣（LeetCode）
 链接：https://leetcode-cn.com/problems/course-schedule
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+https://labuladong.gitee.io/algo/2/20/37/
 */
 
-func canFinish(numCourses int, prerequisites [][]int) bool {
-	pic := make(map[int]struct{})
-	for _, l := range prerequisites {
-		pic[l[0]] = struct{}{}
-		pic[l[1]] = struct{}{}
-	}
-	total := len(pic)
+func Test207(t *testing.T) {
+	t.Log(canFinish(2, [][]int{{1, 0}}))         // true
+	t.Log(canFinish(2, [][]int{{0, 1}}))         // true
+	t.Log(canFinish(2, [][]int{{1, 0}, {0, 1}})) // false
+	t.Log(canFinish(1, [][]int{}))               // true
+}
 
+func canFinishBest(numCourses int, prerequisites [][]int) bool {
+	res := []int{}
+	edges := make([][]int, numCourses)
+	indeg := make([]int, numCourses)
+	for _, y := range prerequisites {
+		edges[y[1]] = append(edges[y[1]], y[0])
+		indeg[y[0]]++
+	}
+	q := []int{}
+	for x, y := range indeg {
+		if y == 0 {
+			q = append(q, x)
+		}
+	}
+	for len(q) > 0 {
+		y := q[0]
+		q = q[1:]
+		res = append(res, y)
+		for _, z := range edges[y] {
+			indeg[z]--
+			if indeg[z] == 0 {
+				q = append(q, z)
+			}
+		}
+	}
+	return len(res) == numCourses
+}
+
+func canFinish(numCourses int, prerequisites [][]int) bool {
+	if len(prerequisites) == 0 {
+		return true
+	}
+	_207_210_init()
+	dictMap = makeDict(prerequisites)
+	visited = make(map[int]struct{})
+	onPath = make(map[int]struct{})
+	for i := 0; i < numCourses; i++ {
+		dfs207(i)
+	}
+	return !hasCycle
+}
+
+func _207_210_init() {
+	hasCycle = false
+	dictMap = make(map[int][]int)
+	visited = make(map[int]struct{})
+	onPath = make(map[int]struct{})
+}
+
+var hasCycle bool
+var dictMap map[int][]int
+var visited map[int]struct{}
+var onPath map[int]struct{}
+
+func makeDict(prerequisites [][]int) map[int][]int {
+	var dictMap map[int][]int
+	dictMap = make(map[int][]int)
+	for _, v := range prerequisites {
+		cour := v[0]
+		need := v[1]
+		cours, ok := dictMap[need]
+		if ok {
+			dictMap[need] = append(cours, cour)
+		} else {
+			dictMap[need] = []int{cour}
+		}
+	}
+	return dictMap
+}
+
+func dfs207(next int) {
+	if _, ok := onPath[next]; ok {
+		hasCycle = true
+		return
+	}
+	if _, ok := visited[next]; ok || hasCycle {
+		return
+	}
+	onPath[next] = struct{}{}
+	for _, n := range dictMap[next] {
+		dfs207(n)
+	}
+	delete(onPath, next)
+	visited[next] = struct{}{}
 }
---
+++ 210_课程表2_test.go
@@ -0,0 +1,104 @@
+package Code
+
+import (
+	"testing"
+)
+
+/**
+现在你总共有 numCourses 门课需要选，记为 0 到 numCourses - 1。给你一个数组 prerequisites ，其中 prerequisites[i] = [ai, bi] ，表示在选修课程 ai 前 必须 先选修 bi 。
+
+例如，想要学习课程 0 ，你需要先完成课程 1 ，我们用一个匹配来表示：[0,1] 。
+返回你为了学完所有课程所安排的学习顺序。可能会有多个正确的顺序，你只要返回 任意一种 就可以了。如果不可能完成所有课程，返回 一个空数组 。
+
+
+
+示例 1：
+
+输入：numCourses = 2, prerequisites = [[1,0]]
+输出：[0,1]
+解释：总共有 2 门课程。要学习课程 1，你需要先完成课程 0。因此，正确的课程顺序为 [0,1] 。
+示例 2：
+
+输入：numCourses = 4, prerequisites = [[1,0],[2,0],[3,1],[3,2]]
+输出：[0,2,1,3]
+解释：总共有 4 门课程。要学习课程 3，你应该先完成课程 1 和课程 2。并且课程 1 和课程 2 都应该排在课程 0 之后。
+因此，一个正确的课程顺序是 [0,1,2,3] 。另一个正确的排序是 [0,2,1,3] 。
+示例 3：
+
+输入：numCourses = 1, prerequisites = []
+输出：[0]
+
+
+提示：
+1 <= numCourses <= 2000
+0 <= prerequisites.length <= numCourses * (numCourses - 1)
+prerequisites[i].length == 2
+0 <= ai, bi < numCourses
+ai != bi
+所有[ai, bi] 互不相同
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/course-schedule-ii
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+func Test210(t *testing.T) {
+	t.Log(findOrder(2, [][]int{{1, 0}}))                         // 0,1
+	t.Log(findOrder(4, [][]int{{1, 0}, {2, 0}, {3, 1}, {3, 2}})) // 0,2,1,3
+	t.Log(findOrder(2, [][]int{{1, 0}, {0, 1}}))                 // 0
+	t.Log(findOrder(1, nil))                                     // 0
+}
+
+/**
+var hasCycle bool
+var dictMap map[int][]int
+var visited map[int]struct{}
+var onPath map[int]struct{}
+*/
+
+func findOrder(numCourses int, prerequisites [][]int) []int {
+	_207_210_init()
+	dictMap = makeDict(prerequisites)
+	visited = make(map[int]struct{})
+	onPath = make(map[int]struct{})
+	curss := make([]int, 0, numCourses-1)
+	for i := 0; i < numCourses; i++ {
+		hasCycle = false
+		dfs210(i, &curss)
+		if hasCycle {
+			return nil
+		}
+	}
+	if len(curss) != numCourses {
+		return nil
+	}
+	i := 0
+	j := len(curss) - 1
+	for i < j {
+		curss[i], curss[j] = curss[j], curss[i]
+		i++
+		j--
+	}
+	return curss
+}
+
+func dfs210(cur int, curss *[]int) {
+	if _, ok := onPath[cur]; ok {
+		hasCycle = true
+		return
+	}
+
+	if _, ok := visited[cur]; ok || hasCycle {
+		return
+	}
+
+	onPath[cur] = struct{}{}
+	for _, next := range dictMap[cur] {
+		dfs210(next, curss)
+	}
+	delete(onPath, cur)
+	if !hasCycle {
+		*curss = append(*curss, cur)
+	}
+	visited[cur] = struct{}{}
+}
