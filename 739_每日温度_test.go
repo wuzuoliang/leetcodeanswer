9660db41f131c00e462b8ac7681d5c634903f343
吴柞良 <wuzuoliang@MacBook-Pro.local>
2022-03-05 23:50:57 +0800

    add test code

---
+++ 739_每日温度_test.go
@@ -0,0 +1,52 @@
+package Code
+
+import "testing"
+
+/**
+给定一个整数数组temperatures，表示每天的温度，返回一个数组answer，其中answer[i]是指在第 i 天之后，才会有更高的温度。如果气温在这之后都不会升高，请在该位置用0 来代替。
+
+
+
+示例 1:
+
+输入: temperatures = [73,74,75,71,69,72,76,73]
+输出:[1,1,4,2,1,1,0,0]
+示例 2:
+
+输入: temperatures = [30,40,50,60]
+输出:[1,1,1,0]
+示例 3:
+
+输入: temperatures = [30,60,90]
+输出: [1,1,0]
+
+
+提示：
+
+1 <=temperatures.length <= 105
+30 <=temperatures[i]<= 100
+
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/daily-temperatures
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+func Test739(t *testing.T) {
+	t.Log(dailyTemperatures([]int{73, 74, 75, 71, 69, 72, 76, 73}))
+}
+
+// https://leetcode-cn.com/problems/daily-temperatures/solution/leetcode-tu-jie-739mei-ri-wen-du-by-misterbooo/
+func dailyTemperatures(temperatures []int) []int {
+	n := len(temperatures)
+	res := make([]int, n)
+	stack := make([]int, 0, n)
+	for i := 0; i < n; i++ {
+		for len(stack) > 0 && temperatures[i] > temperatures[stack[len(stack)-1]] {
+			topValue := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			res[topValue] = i - topValue
+		}
+		stack = append(stack, i)
+	}
+	return res
+}
