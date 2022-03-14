08cf71eeac8f557c399a38a1ebf34bb9ebf0d586
吴柞良 <wuzuoliang@1LWUZUOLIANG.local>
2022-03-14 18:46:27 +0800

    add test code

---
+++ 1135_最低成本联通所有城市_test.go
@@ -0,0 +1,11 @@
+package Code
+
+import "testing"
+
+func Test1135(t *testing.T) {
+	t.Log(minimumCost(3, [][]int{{1, 2, 5}, {1, 3, 6}, {2, 3, 1}})) //6
+}
+
+func minimumCost(n int, connections [][]int) int {
+	return 0
+}
