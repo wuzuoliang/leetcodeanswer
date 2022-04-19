5c81be2108750c6844b241333eb114009614fddd
wuzuoliang <287915111@qq.com>
2022-04-19 10:44:42 +0800

    add test code

---
+++ 386_字典序排数_test.go
@@ -0,0 +1,51 @@
+package Code
+
+import "testing"
+
+/**
+给你一个整数 n ，按字典序返回范围 [1, n] 内所有整数。
+
+你必须设计一个时间复杂度为 O(n) 且使用 O(1) 额外空间的算法。
+
+
+
+示例 1：
+
+输入：n = 13
+输出：[1,10,11,12,13,2,3,4,5,6,7,8,9]
+示例 2：
+
+输入：n = 2
+输出：[1,2]
+
+
+提示：
+
+1 <= n <= 5 * 104
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/lexicographical-numbers
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+func Test386(t *testing.T) {
+	t.Log(lexicalOrder(13))
+	t.Log(lexicalOrder(23))
+
+}
+
+func lexicalOrder(n int) []int {
+	ans := make([]int, n)
+	num := 1
+	for i := range ans {
+		ans[i] = num
+		if num*10 <= n {
+			num *= 10
+		} else {
+			for num%10 == 9 || num+1 > n {
+				num /= 10
+			}
+			num++
+		}
+	}
+	return ans
+}
